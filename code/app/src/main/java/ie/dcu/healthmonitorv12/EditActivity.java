28bf979d0a0c563f8ca70a769d2a481467cf17ef
James Toolen <james.toolen2@mail.dcu.ie>
2019-03-04 15:37:23 +0000

    Revert "refactor"
    This reverts commit 20658f9f9dfdf7ffbb4edd233291ee752907c0a0

--- code/app/src/main/java/ie/dcu/healthmonitorv12/EditActivity.java
+++ code/app/src/main/java/ie/dcu/healthmonitorv12/EditActivity.java
@@ -1,27 +1,19 @@
 package ie.dcu.healthmonitorv12;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
 class EditActivity extends Activity {
 
-    private TextView heightLabel;
     private EditText heightText;
-    private TextView weightLabel;
     private EditText weightText;
     private TextView resultView;
-	private int height;
-	private int weight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -32,39 +24,23 @@ class EditActivity extends Activity {
         enterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public  void onClick(View view) {
-                calcBmi();
+                heightText = (EditText) findViewById(R.id.height_text);
+                weightText = (EditText) findViewById(R.id.weight_text);
+                resultView = (TextView) findViewById(R.id.bmi);
+
+                int height = Integer.parseInt(heightText.getText().toString());
+                int weight = Integer.parseInt(weightText.getText().toString());
+                DecimalFormat value = new DecimalFormat("#.#");
+                String result = value.format((weight / (Math.pow(height, 2)))*10000);
+                String finalStr = "Body Mass Index\n" + result + "\n";
+                if(((weight / (Math.pow(height, 2)))*10000) < 18.5)
+                    finalStr = finalStr + "Underweight";
+                else if(((weight / (Math.pow(height, 2)))*10000) > 24.9)
+                    finalStr = finalStr + "Overweight";
+                else
+                    finalStr = finalStr + "Good Ratio";
+                resultView.setText(finalStr);
             }
         });
-
     }
-
-    private void calcBmi(){
-        heightLabel = (TextView) findViewById(R.id.height_text_label);
-        heightText = (EditText) findViewById(R.id.height_text);
-        weightLabel = (TextView) findViewById(R.id.weight_text_label);
-        weightText = (EditText) findViewById(R.id.weight_text);
-        resultView = (TextView) findViewById(R.id.bmi);
-
-        heightLabel.setText(heightText.getText().toString() + " cms");
-        weightLabel.setText(weightText.getText().toString() + " kgs");
-        height = Integer.parseInt(heightText.getText().toString());
-        weight = Integer.parseInt(weightText.getText().toString());
-
-        DecimalFormat value = new DecimalFormat("#.#");
-        String result = value.format((weight / (Math.pow(height, 2)))*10000);
-        String finalStr = "Body Mass Index\n" + result + "\n";
-        if(((weight / (Math.pow(height, 2)))*10000) < 18.5)
-            finalStr = finalStr + "Underweight";
-        else if(((weight / (Math.pow(height, 2)))*10000) > 24.9)
-            finalStr = finalStr + "Overweight";
-        else
-            finalStr = finalStr + "Good Ratio";
-        resultView.setText(finalStr);
-    }
-
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
-    }
-
 }
